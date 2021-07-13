39d6d7439d1e0e6674b49a687d82033fda300979
pengliang <624151713@qq.com>
2021-07-13 23:42:29 +0800

    add sign

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <span class="github"><a href="https://github.com/PL-FE/bpmn-doc/tree/dev"
+        target="_blank">bpmn-doc - dev分支</a></span>
     <router-view />
   </div>
 </template>
@@ -8,5 +10,11 @@
 body {
   margin: 0;
   overflow: hidden;
+}
+.github {
+  position: fixed;
+  transform: translateX(-50%);
+  left: 50%;
+  z-index: 9;
 }
 </style>
